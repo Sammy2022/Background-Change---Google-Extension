ca83f4bd8d9ae9b2529cdac76845d32b6d4a8a64
Sammy2022 <86789353+Sammy2022@users.noreply.github.com>
2022-01-10 23:40:27 +0530

    Add files via upload

---
+++ bg_change/options.js
@@ -0,0 +1,49 @@
+let page = document.getElementById("buttonDiv");
+let selectedClassName = "current";
+const presetButtonColors = [ "#3aa757", "#e8453c","#f9bb2d","#4688f1"];
+
+function handleButtonClick(event) {
+
+   let current = event.target.parentElement.querySelector(
+       `.${selectedClassName}`
+     );
+     if ( current && current !== event.target) {
+        current.classList.remove(selectedClassName);
+    }
+
+    let color = event.target.dataset.color;
+    event.target.classList.add(selectedClassName);
+    chrome.storage.sync.set({ color });
+
+
+
+}  
+
+
+
+// Add a button to the page for each supplied color
+
+function constructOptions(buttonColors) {
+    chrome.storage.sync.get("color", (data) => {
+        let currentColor = data.color;
+        // For each color we were provided...
+        for (let buttonColor of buttonColors) {
+            // Create a button with that color
+            let button = document.createElement("button");
+            button.dataset.color = buttonColor;
+            button.style.backgroundColor = buttonColor;
+
+            // Mark the currently selected color
+
+            if (buttonColor === currentColor) {
+                button.classList.add(selectedClassName);
+            }
+            // register a listener for when that button is clicked
+            button.addEventListener("click", handleButtonClick);
+            page.appendChild(button);
+        }
+    });
+}
+// Initialize the page by constructing the color options
+
+constructOptions(presetButtonColors);
